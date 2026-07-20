05627870b1e5e9e30302e286348edb13307659d2
TojoRAK <tojomarofitahiana@gmail.com>
2026-07-20 09:40:47 +0300

    [config] database

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -26,11 +26,11 @@ class Database extends Config
      */
     public array $default = [
         'DSN'          => '',
-        'hostname'     => '127.0.0.1',
-        'username'     => 'root',
+        'hostname'     => '',
+        'username'     => '',
         'password'     => '',
-        'database'     => 'biblio',
-        'DBDriver'     => 'MySQLi',
+        'database'     => ROOTPATH . 'base.db',
+        'DBDriver'     => 'SQLite3',
         'DBPrefix'     => '',
         'pConnect'     => false,
         'DBDebug'      => true,
